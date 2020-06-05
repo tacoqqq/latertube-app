5408872c842577f768f35e04ae6462669fed6f53
daphne Fang <daphneys.fang@gmail.com>
2020-06-05 02:49:04 -0700

    adjusted logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,7 +10,7 @@ class Header extends Component {
             <Switch>
                 <Route exact path="/" render={props => {
                     return <nav role="navigation">
-                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/app-logo-lg.png')} /><NavLink to="/">LaterTube</NavLink></div>
+                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/apple-touch-icon.png')} /><NavLink to="/">LaterTube</NavLink></div>
                         <ul className="nav-menu">
                             <li><NavLink to="/home">Demo</NavLink></li>
                         </ul>
@@ -18,7 +18,7 @@ class Header extends Component {
                 }}/>
                 <Route exact path="/home" render={props => {
                     return <nav role="navigation">
-                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/app-logo-lg.png')} /><NavLink to="/">LaterTube</NavLink></div>
+                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/apple-touch-icon.png')} /><NavLink to="/">LaterTube</NavLink></div>
                         <ul className="nav-menu">
                             <li className="nav-menu-item"><NavLink to="/home">Home</NavLink></li>
                             <li className="nav-menu-item"><NavLink to="/add-genre">+ Genre</NavLink></li>
@@ -28,7 +28,7 @@ class Header extends Component {
                 }} />
                 <Route exact path="/add-genre" render={props => {
                     return <nav role="navigation">
-                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/app-logo-lg.png')} /><NavLink to="/">LaterTube</NavLink></div>
+                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/apple-touch-icon.png')} /><NavLink to="/">LaterTube</NavLink></div>
                         <ul className="nav-menu">
                             <li className="nav-menu-item"><NavLink to="/home">Home</NavLink></li>
                             <li className="nav-menu-item"><NavLink to="/add-genre">+ Genre</NavLink></li>
@@ -38,7 +38,7 @@ class Header extends Component {
                 }} />
                 <Route exact path="/add-video" render={props => {
                     return <nav role="navigation">
-                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/app-logo-lg.png')} /><NavLink to="/">LaterTube</NavLink></div>
+                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/apple-touch-icon.png')} /><NavLink to="/">LaterTube</NavLink></div>
                         <ul className="nav-menu">
                             <li className="nav-menu-item"><NavLink to="/home">Home</NavLink></li>
                             <li className="nav-menu-item"><NavLink to="/add-genre">+ Genre</NavLink></li>
@@ -48,7 +48,7 @@ class Header extends Component {
                 }} />
                 <Route exact path="/videos/:videoId/edit" render={props => {
                     return <nav role="navigation">
-                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/app-logo-lg.png')} /><NavLink to="/">LaterTube</NavLink></div>
+                        <div className="nav-title"><img className="app-logo" alt="app logo" src={require('../../img/apple-touch-icon.png')} /><NavLink to="/">LaterTube</NavLink></div>
                         <ul className="nav-menu">
                             <li className="nav-menu-item"><NavLink to="/home">Home</NavLink></li>
                             <li className="nav-menu-item"><NavLink to="/add-genre">+ Genre</NavLink></li>
